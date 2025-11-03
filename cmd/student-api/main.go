edf73e8c8bad91f99c8fa72061f9266e0f673c69
manish-npx <mkdablu@gmail.com>
2025-11-03 11:13:29 +0530

    optimize main.go production ready

--- cmd/student-api/main.go
+++ cmd/student-api/main.go
@@ -13,61 +13,51 @@ import (
 	"github.com/manish-npx/go-student-api/internal/config"
 )
 
-// load config
-// load database
-// setup server
-// setup route
 func main() {
-	//load config
+	// Load config
 	cfg := config.MustLoad()
 
-	//load database
+	// Load database (TODO)
 
-	//setup route
+	// Setup routes
 	route := http.NewServeMux()
 	route.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/" {
 			http.NotFound(w, r)
 			return
 		}
-		//fmt.Fprintf(w, "Welcome to the home page!!  ")
 		w.Write([]byte("Welcome to student api"))
 	})
 
-	//setup server
-	server := http.Server{
+	// Setup server
+	server := &http.Server{
 		Addr:    cfg.HttpServer.Addr,
 		Handler: route,
 	}
 
-	log.Printf("Server started on Addr %s", cfg.HttpServer.Addr)
+	slog.Info("Server started", slog.String("address", cfg.HttpServer.Addr))
 
-	//channel signal
+	// Channel for graceful shutdown
 	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
-	signal.Notify(done, os.Interrupt, syscall.SIGILL, syscall.SIGTERM)
-
+	// Start server in background
 	go func() {
-
-		err := server.ListenAndServe()
-		if err != nil {
-			log.Fatal("failed to start server")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("❌ Failed to start server: %v", err)
 		}
 	}()
 
-	<-done
+	<-done // Block until shutdown signal
 
-	slog.Info("Shutting down server ")
+	slog.Info("📴 Shutting down server...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-
 	defer cancel()
 
-	err := server.Shutdown(ctx)
-
-	if err != nil {
-		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("❌ Failed to gracefully shutdown server", slog.String("error", err.Error()))
+	} else {
+		slog.Info("✅ Server shutdown successfully")
 	}
-	slog.Info("server shutdown successfully")
-
 }
